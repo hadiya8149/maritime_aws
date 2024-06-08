972a9144370cce5a7e54f384139d5f637cb9f51d
hadiya8149 <hadiya8149@gmail.com>
2024-06-08 11:55:13 +0500

    redeploy

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -2,22 +2,33 @@ import mysql from 'mysql'
 import dotenv from 'dotenv';
 dotenv.config();
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
   
-});
+// });
+import pg from 'pg';
 
-connection.connect(function (err) {
-    if (err) {
-        console.error('Error connecting to the database:', err.message);
-        throw err;
-    }
+const { Pool } = pg;
 
-    console.log( "Database connected successfully!");
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+})
+pool.connect((err)=>{
+  if(err) throw err
+  console.log("Connected to PostgreSQL successfully")
+})
+// connection.connect(function (err) {
+//     if (err) {
+//         console.error('Error connecting to the database:', err.message);
+//         throw err;
+//     }
 
-});
+//     console.log( "Database connected successfully!");
 
-export const db = connection;
+// });
+
+export const db = pool;
+// export const db = connection;
